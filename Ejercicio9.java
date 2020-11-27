47bf6f60c9e609cd7baed44797bf33aeaf4d8689
Ivan Gil Conde <ivanbrst2@gmail.com>
2020-11-27 20:26:56 +0100

    Definidas Variables Ejercicio9

---
+++ Ejercicio9.java
@@ -0,0 +1,42 @@
+import java util.Scanner;
+
+public class Ejercicio9{
+	
+	public static void main (String args[]){
+		
+		int alto;
+		int longo;
+		
+		Scanner a=new Scanner(System.in);
+		System.out.println("Introduzca o número");
+		
+		longo=a.nextInt();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
