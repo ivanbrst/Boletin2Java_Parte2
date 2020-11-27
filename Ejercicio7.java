fdc56ac7c2a8263091a1194baaff4660fe68b5e0
Ivan Gil Conde <ivanbrst2@gmail.com>
2020-11-27 18:18:41 +0100

    Terminado Ejercicio7

---
+++ Ejercicio7.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ejercicio7{
+	
+	public static void main (String args[]){
+		
+		int n;
+		int i;
+		int j;
+		
+		Scanner a=new Scanner(System.in);
+		System.out.println("Introduzca un número entre 3 e 7");
+		
+		n=a.nextInt();
+		
+		if((n>=3) && (n<=7))
+			{for(i=1; i<=n; i++)
+				{for (j=0; j<i; j++)
+					{System.out.print("*");}
+				System.out.println("");}
+				
+			}
+			
+		else{System.out.println("Error");}
+		
+	}
+}
