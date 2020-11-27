ee96569f124236bf0aaee0284acb674e975e05de
Ivan Gil Conde <ivanbrst2@gmail.com>
2020-11-27 15:55:33 +0100

    Añadidas Variables Ejercicio6

---
+++ Ejercicio6.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Ejercicio6{
+	
+	public static void main (String args[]){
+		
+		int longo;
+		int ancho;
+		
+		int contador;
+		
+		
+		Scanner a=new Scanner(System.in);
+		System.out.println("Introduzca o longo que desexe");
+		longo=a.nextInt();
+		
+		System.out.println("Introduzca o ancho que desexe");
+		ancho=a.nextInt();
+		
+	}
+}
