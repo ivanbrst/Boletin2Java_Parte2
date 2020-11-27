98baaf97d20cb8df15d25b5edf14be17f7a76fd1
Ivan Gil Conde <ivanbrst2@gmail.com>
2020-11-27 18:29:16 +0100

    Añadidas Variables Ejercicio8

---
+++ Ejercicio8.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ejercicio8{
+	
+	public static void main(String args[]){
+		
+		int largo;
+		int alto;
+		
+		Scanner a=new Scanner(System.in);
+		System.out.println("Introduzca un número entre 3 e 10");
+		
+		alto=a.nextInt();
+		
+		largo=alto*2;
+
+
+
+		
+	}
+}
+
+	
