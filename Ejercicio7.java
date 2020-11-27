914bd938cb6092b4a56f1199e4ab19c43e8a6993
Ivan Gil Conde <ivanbrst2@gmail.com>
2020-11-27 17:36:33 +0100

    Definidas Variables Ejercicio7

---
+++ Ejercicio7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio7{
+    
+    public static void main (String args[]){
+        
+        int n;
+        int contador;
+        
+        Scanner a=new Scanner(System.in);
+        System.out.println("Introduzca un número");
+        
+        n=a.nextInt();
+       
+        
+    }
+}
