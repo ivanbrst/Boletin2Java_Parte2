cc66c3998e30e580966237f662a23f8cbc1ca2cd
Ivan Gil Conde <ivanbrst2@gmail.com>
2020-11-27 16:46:28 +0100

    Terminado Ejercicio6

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -5,31 +5,45 @@ public class Ejercicio6{
 	
 	public static void main (String args[]){
 		
-		int ancho;
 		int longo;
+		int alto;
 		
 		int contador;
+		int saltos;
 		
 		Scanner a=new Scanner(System.in);
 		System.out.println("Introduzca o longo que desexe");
 		longo=a.nextInt();
 		
-		System.out.println("Introduzca o ancho que desexe");
-		ancho=a.nextInt();
+		System.out.println("Introduzca o alto que desexe");
+		alto=a.nextInt();
 		
 		contador=0;
 		
-		while(contador<ancho)
+		while(contador<longo)
 			{System.out.print("*");
 			contador++;}
 		
 		System.out.println("");
+		
+		contador=0;
+		saltos=0;
+		
+		while(saltos<alto-2)
+			{while(contador<longo)
+				{if((contador==0) | (contador==longo-1))
+					{System.out.print("*");}
+				else{System.out.print(" ");}
+				contador++;}
+				contador=0;
+				saltos++;
+				System.out.println("");}
 				
 		contador=0;
 		
-		while(contador<ancho)
-		{System.out.print("*");
-		contador++;}
+		while(contador<longo)
+			{System.out.print("*");
+			contador++;}
 			
 		System.out.println("");
 		
