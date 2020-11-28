86d389dcd7b04f5680c5ed4bff7c5faad33e8701
Ivan Gil Conde <ivanbrst2@gmail.com>
2020-11-28 13:59:55 +0100

    Ejercicio9. Parte de arriba hecha

--- Ejercicio9.java
+++ Ejercicio9.java
@@ -1,4 +1,4 @@
-import java util.Scanner;
+import java.util.Scanner;
 
 public class Ejercicio9{
 	
@@ -6,12 +6,44 @@ public class Ejercicio9{
 		
 		int alto;
 		int longo;
+		int contaf;
+		int contac;
 		
 		Scanner a=new Scanner(System.in);
 		System.out.println("Introduzca o número");
 		
 		longo=a.nextInt();
 		
+		alto=longo;
+		
+		if((longo>3)&&(longo<15)&&(longo%2!=0))
+			{
+				
+				
+				//Parte de arriba
+				
+				for(contaf=0; contaf<longo/2+1; contaf++)
+				
+					
+					{for(contac=1; contac<=longo; contac++)
+					
+						{if((contac==longo/2+1+contaf) | (contac==longo/2+1-contaf))
+							{System.out.print("*");}
+						else{System.out.print(" ");}
+						}
+						System.out.println("");}
+						
+			
+						
+						
+			}
+			else{System.out.println("Error");}
+		
+					
+					
+					
+			
+	
 		
 		
 		
