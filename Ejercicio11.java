d89c72ea16cb6d4ea20272c65339fa8c1bb523be
Ivan Gil Conde <ivanbrst2@gmail.com>
2020-11-29 13:10:16 +0100

    Definidas Variables Ejercicio11

---
+++ Ejercicio11.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ejercicio11{
+    
+
+    public static void main (String args[]){
+        
+
+        int dia;
+        int mes;
+        int ano;
+        int suma;
+        
+        Scanner a=new Scanner(System.in);
+        System.out.println("Introduzca o día que naceu"); 
+        dia=a.nextInt();
+               
+        System.out.println("Introduzca o mes que naceu");
+        mes=a.nextInt();
+        
+        System.out.println("Introduzca o ano que naceu");
+        ano=a.nextInt();
+        
+
+
+    }
+}
+
